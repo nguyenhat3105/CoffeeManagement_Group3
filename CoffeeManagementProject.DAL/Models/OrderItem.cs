4d7cdf2a60d506345453be54c848119f7ae832a3
nguyenhat3105 <nguyencongnhat3105@gmail.com>
2025-11-13 09:56:10 +0700

    update UI

---
+++ CoffeeManagementProject.DAL/Models/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoffeeManagementProject.DAL.Models;
+
+public partial class OrderItem
+{
+    public int Id { get; set; }
+
+    public int OrderId { get; set; }
+
+    public int MenuItemId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public virtual MenuItem MenuItem { get; set; } = null!;
+
+    [NotMapped] // Đảm bảo Entity Framework không cố gắng map thuộc tính này vào DB
+    public decimal TotalPrice
+    {
+        get
+        {
+            // Tính toán Thành tiền: Số lượng * Giá đơn vị (lấy từ MenuItem)
+            return Quantity * (MenuItem?.Price ?? 0);
+        }
+    }
+
+    public virtual Order Order { get; set; } = null!;
+}
---
+++ CoffeeManagementProject.DAL/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoffeeManagementProject.DAL.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+    [NotMapped]
+    public string FullName
+    {
+        get
+        {
+            return $"{FirstName} {LastName}"; // Hoặc (FirstName + " " + LastName)
+        }
+    }
+
+    public byte RoleId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<Order> OrderCustomers { get; set; } = new List<Order>();
+
+    public virtual ICollection<Order> OrderStaffs { get; set; } = new List<Order>();
+
+    public virtual Role Role { get; set; } = null!;
+}
